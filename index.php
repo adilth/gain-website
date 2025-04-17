39e89146e1e3c327727beeb70f3784be43911170
adilth <rajaadil1995@gmail.com>
2025-04-17 14:57:45 +0100

    fix problem with the images path and menu

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <link rel="icon" type="image/svg+xml" href="./gain-logo.svg" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Gain</title>
-    <script type="module" crossorigin src="./dist/aassets/index-CN4wGx77.js"></script>
+    <script type="module" crossorigin src="./dist/assets/index-CN4wGx77.js"></script>
     <link rel="stylesheet" crossorigin href="./dist/assets/index-Clb0vIrx.css">
 </head>
 
