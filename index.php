e39107ef98c4a7ece547761eaf52c99b63167fc2
Adil tahiri <rajaadil1995@gmail.com>
2025-06-29 19:45:03 +0100

    remove bug

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
     <!-- Favicon -->
     <link rel="apple-touch-icon" sizes="180x180" href="/dist/apple-touch-icon.png" />
     <link rel="icon" type="image/png" sizes="32x32" href="/dist/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="/dist/favicon-16x16.png" />00
+    <link rel="icon" type="image/png" sizes="16x16" href="/dist/favicon-16x16.png" />
     <link rel="manifest" href="/dist/site.webmanifest" />
 
     <!-- Mobile Meta -->
